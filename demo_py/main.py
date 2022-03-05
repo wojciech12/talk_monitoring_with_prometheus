5f1ebc13e699736225ae7900825aa1abb86ceaac
wojciech11 <wojciech.barczynski@wsb.wroclaw.pl>
2022-03-05 14:49:58 +0100

    Move from Summary to Histogram

--- demo_py/main.py
+++ demo_py/main.py
@@ -1,21 +1,19 @@
 from flask import Flask, Response
 from flask import request
-from prometheus_client import (Summary, Histogram, generate_latest,
-                               CollectorRegistry)
+from prometheus_client import Histogram, generate_latest, CollectorRegistry
 from prometheus_client import multiprocess
 import ast
 import time
 
 
 class MetricCollector:
-
     def observe_myself(self, path, method, status_code, duration):
-        self._me.labels(path=path, method=method,
-                        status_code=status_code).observe(duration)
+        self._me.labels(path=path, method=method, status_code=status_code).observe(
+            duration
+        )
 
     def observe_db(self, status_code, sql_state, duration):
-        self._db.labels(status_code=status_code,
-                        sql_state=sql_state).observe(duration)
+        self._db.labels(status_code=status_code, sql_state=sql_state).observe(duration)
 
     def observe_external(self, status_code, duration):
         self._external.labels(status_code=status_code).observe(duration)
@@ -26,22 +24,30 @@ class MetricCollector:
     @staticmethod
     def newCollector(service_name):
         prefix = service_name.replace("-", "_")
-        STATUS_CODE = 'status_code'
-        PATH = 'path'
-        HTTP_METHOD = 'method'
+        STATUS_CODE = "status_code"
+        PATH = "path"
+        HTTP_METHOD = "method"
 
-        about_me = Summary(prefix + "_duration_seconds",
-                           service_name + " latency request distribution",
-                           [PATH, HTTP_METHOD, STATUS_CODE])
+        about_me = Histogram(
+            prefix + "_duration_seconds",
+            service_name + " latency request distribution",
+            [PATH, HTTP_METHOD, STATUS_CODE],
+            buckets=(0.1, 0.25, 0.5, 0.75, 0.90, 1.0, 2.5),
+        )
 
-        about_db = Histogram(prefix + "_database_duration_seconds",
-                             "database latency request distribution",
-                             [STATUS_CODE, 'sql_state'],
-                             buckets=(.1, .25, .5, .75, .90, 1.0, 2.5))
+        about_db = Histogram(
+            prefix + "_database_duration_seconds",
+            "database latency request distribution",
+            [STATUS_CODE, "sql_state"],
+            buckets=(0.1, 0.25, 0.5, 0.75, 0.90, 1.0, 2.5),
+        )
 
-        about_her = Summary(prefix + "_audit_duration_seconds",
-                            "audit service latency request distribution",
-                            [STATUS_CODE])
+        about_her = Histogram(
+            prefix + "_audit_duration_seconds",
+            "audit service latency request distribution",
+            [STATUS_CODE],
+            buckets=(0.1, 0.25, 0.5, 0.75, 0.90, 1.0, 2.5),
+        )
 
         mc = MetricCollector()
         mc._me = about_me
@@ -54,22 +60,22 @@ def get_collector(name):
     return MetricCollector.newCollector(name)
 
 
-def add_routes(app,  collector):
-    @app.route('/hello')
+def add_routes(app, collector):
+    @app.route("/hello")
     def hello_route():
-        return 'hello'
+        return "hello"
 
-    @app.route('/world')
+    @app.route("/world")
     def world_route():
-        return 'world'
+        return "world"
 
-    @app.route('/complex')
+    @app.route("/complex")
     def complex_operation():
         # getting data from db
         try:
             work_with_db(collector)
         except RuntimeError as re:
-            r = Response("{0}".format(re), mimetype='text/plain')
+            r = Response("{0}".format(re), mimetype="text/plain")
             r.status_code = 503
             return r
 
@@ -77,10 +83,10 @@ def add_routes(app,  collector):
         try:
             work_with_third_party(collector)
         except RuntimeError as re:
-            r = Response("{0}".format(re), mimetype='text/plain')
+            r = Response("{0}".format(re), mimetype="text/plain")
             r.status_code = 503
             return r
-        return 'Success!'
+        return "Success!"
 
 
 def work_with_db(collector):
@@ -92,12 +98,12 @@ def work_with_db(collector):
     try:
         call_db(db_sleep, is_db_error)
         latency = time.time() - start_time
-        collector.observe_db('0', '0', latency)
+        collector.observe_db("0", "0", latency)
     except RuntimeError as re:
         """
         """
         latency = time.time() - start_time
-        collector.observe_db('1001', 'HY000', latency)
+        collector.observe_db("1001", "HY000", latency)
         raise re
 
 
@@ -113,12 +119,12 @@ def work_with_third_party(collector):
     try:
         call_external(srv_sleep, is_srv_error)
         latency = time.time() - start_time
-        collector.observe_external('200', latency)
+        collector.observe_external("200", latency)
     except RuntimeError as re:
         """
         """
         latency = time.time() - start_time
-        collector.observe_external('500', latency)
+        collector.observe_external("500", latency)
         raise re
 
 
@@ -140,12 +146,11 @@ def instrument_requests(app, collector):
         request.start_time = time.time()
 
     def after(response):
-        if request.path != '/metrics':
+        if request.path != "/metrics":
             request_latency = time.time() - request.start_time
-            collector.observe_myself(request.path,
-                                     request.method,
-                                     response.status_code,
-                                     request_latency)
+            collector.observe_myself(
+                request.path, request.method, response.status_code, request_latency
+            )
         return response
 
     app.before_request(before)
@@ -153,11 +158,11 @@ def instrument_requests(app, collector):
 
 
 def add_metrics_route(app, collector):
-    @app.route('/metrics', strict_slashes=False, methods=['GET'])
+    @app.route("/metrics", strict_slashes=False, methods=["GET"])
     def metrics_rotue():
         # txt = generate_latest()
         txt = get_latest_when_running_with_gunicorn()
-        return Response(txt, mimetype='text/plain')
+        return Response(txt, mimetype="text/plain")
 
 
 # you can skip *multiprocess_mode*
@@ -171,7 +176,7 @@ def get_latest_when_running_with_gunicorn():
 
 def get_app():
     app = Flask(__name__)
-    c = get_collector('order-mgmt')
+    c = get_collector("order-mgmt")
     add_routes(app, c)
     add_metrics_route(app, c)
     instrument_requests(app, c)
@@ -180,5 +185,4 @@ def get_app():
 
 if __name__ == "__main__":
     app = get_app()
-    app.run(host='0.0.0.0',
-            port=8080)
+    app.run(host="0.0.0.0", port=8080)
